338b44947e0862d6606c890b871400f3ab0a81cb
user <Abdyralievkairat@gmail.com>
2025-01-31 19:39:36 +0600

    Gift commit

--- src/main/java/sign_up/SignUp.java
+++ src/main/java/sign_up/SignUp.java
@@ -2,7 +2,6 @@ package sign_up;
 
 public class SignUp {
     public static void main(String[] args) {
-        System.out.println("Sign up" +
-                "");
+        System.out.println("Sign up");
     }
 }
