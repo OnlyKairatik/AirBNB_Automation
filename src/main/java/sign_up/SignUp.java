5e736092d47e480820fdc89e2de5dc86c0619d96
user <Abdyralievkairat@gmail.com>
2025-02-01 20:00:38 +0600

    kairat

--- src/main/java/sign_up/SignUp.java
+++ src/main/java/sign_up/SignUp.java
@@ -3,5 +3,7 @@ package sign_up;
 public class SignUp {
     public static void main(String[] args) {
         System.out.println("Sign up");
+        System.out.println("ddddd");
+        System.out.println("kairat");
     }
 }
