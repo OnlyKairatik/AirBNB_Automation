ecec51f366a5a43e24c80d1e175bcf22800e3cf8
OnlyKairatik <abdyralievkairat@gmail.com>
2025-02-01 19:45:12 +0600

    Update Gift.java
    deleted extra lines

--- src/main/java/gift/Gift.java
+++ src/main/java/gift/Gift.java
@@ -3,7 +3,7 @@ package gift;
 public class Gift {
     public static void main(String[] args) {
         System.out.println("Gift for me");
-         System.out.println("Gift for me");
-        System.out.println("Hello");
+
+
     }
 }
