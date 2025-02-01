4b2a1fbeb6c518dc49d61dd59465d596603809e6
user <Abdyralievkairat@gmail.com>
2025-02-01 19:44:59 +0600

    added new sout

--- src/main/java/gift/Gift.java
+++ src/main/java/gift/Gift.java
@@ -4,5 +4,6 @@ public class Gift {
     public static void main(String[] args) {
         System.out.println("Gift for me");
          System.out.println("Gift for me");
+        System.out.println("Hello");
     }
 }
